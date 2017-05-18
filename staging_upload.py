8a08e140fbd0b9f897a1cde1fba744c685665c4a
Johan Lorenzo <jlorenzo@mozilla.com>
2017-05-18 19:24:35 +0200

    Implement upload()

--- staging_upload.py
+++ staging_upload.py
@@ -5,8 +5,10 @@
 
 import aiohttp
 import asyncio
-import logging
+import datetime
 import hashlib
+import json
+import logging
 import pprint
 import os
 import re
@@ -14,24 +16,37 @@ import sys
 import tempfile
 
 from mozapkpublisher.get_apk import check_apk_against_checksum_file as check_mar_against_checksum_file
+from mozapkpublisher.utils import file_sha512sum
+from beetmoverscript.script import upload_to_s3, move_beets, generate_beetmover_manifest
 from scriptworker.utils import download_file
-from taskcluster.async import Queue
 
 log = logging.getLogger(__name__)
-task_graph_id = "d996zZnrTJKne2LHXUTP6w"
-signing_task_names = ["signing-linux-devedition-nightly/opt"]
-num_expected_signing_tasks = 1
 max_concurrent_aiohttp_streams = 10
-# map to copy artifacts to bucket - public/build/target.complete.mar
+BUILD_NUMBER = 1    # XXX Edit this value
+VERSION = '54.0b1'  # XXX Edit this value
+
+CONFIG_FILE = os.path.join(os.path.dirname(__file__), 'config.json')
 
 
 LATEST_MAR_LOCATIONS = {
-    'win32': 'https://index.taskcluster.net/v1/task/gecko.v2.jamun.latest.devedition.win32-opt/artifacts/public/build/firefox-54.0.en-US.win32.complete.mar',
-    'win64': 'https://index.taskcluster.net/v1/task/gecko.v2.jamun.latest.devedition.win64-opt/artifacts/public/build/firefox-54.0.en-US.win64.complete.mar',
-    'macosx64': 'https://index.taskcluster.net/v1/task/gecko.v2.jamun.latest.devedition.macosx64-opt/artifacts/public/build/firefox-54.0.en-US.mac.complete.mar',
-    'linux': 'https://index.taskcluster.net/v1/task/gecko.v2.jamun.latest.devedition-l10n.linux-opt.en-US/artifacts/public/build/update/target.complete.mar',
-    'linux64': 'https://index.taskcluster.net/v1/task/gecko.v2.jamun.latest.devedition-l10n.linux64-opt.en-US/artifacts/public/build/update/target.complete.mar',
+    # 'win32': 'https://index.taskcluster.net/v1/task/gecko.v2.jamun.latest.devedition.win32-opt/artifacts/public/build/firefox-54.0.en-US.win32.complete.mar',
+    # 'win64': 'https://index.taskcluster.net/v1/task/gecko.v2.jamun.latest.devedition.win64-opt/artifacts/public/build/firefox-54.0.en-US.win64.complete.mar',
+    'mac': 'https://index.taskcluster.net/v1/task/gecko.v2.jamun.latest.devedition.macosx64-opt/artifacts/public/build/firefox-54.0.en-US.mac.complete.mar',
+    'linux-i686': 'https://index.taskcluster.net/v1/task/gecko.v2.jamun.latest.devedition-l10n.linux-opt.en-US/artifacts/public/build/update/target.complete.mar',
+    # 'linux-x86_64': 'https://index.taskcluster.net/v1/task/gecko.v2.jamun.latest.devedition-l10n.linux64-opt.en-US/artifacts/public/build/update/target.complete.mar',
 }
+
+ALL_LOCALES = (
+  'ach', 'af', 'an', 'ar', 'as', 'ast', 'az', 'bg', 'bn-BD', 'bn-IN', 'br', 'bs',
+  'ca', 'cak', 'cs', 'cy', 'da', 'de', 'dsb', 'el', 'en-GB', 'en-US', 'en-ZA',
+  'eo', 'es-AR', 'es-CL', 'es-ES', 'es-MX', 'et', 'eu', 'fa', 'ff', 'fi', 'fr',
+  'fy-NL', 'ga-IE', 'gd', 'gl', 'gn', 'gu-IN', 'he', 'hi-IN', 'hr', 'hsb', 'hu',
+  'hy-AM', 'id', 'is', 'it', 'ja-JP-mac', 'ka', 'kab', 'kk', 'km', 'kn', 'ko',
+  'lij', 'lt', 'lv', 'mai', 'mk', 'ml', 'mr', 'ms', 'nb-NO', 'nl', 'nn-NO', 'or',
+  'pa-IN', 'pl', 'pt-BR', 'pt-PT', 'rm', 'ro', 'ru', 'si', 'sk', 'sl', 'son',
+  'sq', 'sr', 'sv-SE', 'ta', 'te', 'th', 'tr', 'uk', 'ur', 'uz', 'vi', 'xh',
+  'zh-CN', 'zh-TW',
+)
 
 def get_filtered(graph):
     filtered = [
@@ -75,20 +90,96 @@ def get_filename_from_url(url):
 
 
 def checksums(context, abs_path_per_platform):
+    path_with_sha512sums_per_platform = {}
+
     for platform, abs_paths in abs_path_per_platform.items():
-        check_mar_against_checksum_file(abs_paths['mar'], abs_paths['checksums'])
+        mar_path = abs_paths['mar']
+        check_mar_against_checksum_file(mar_path, abs_paths['checksums'])
+
+        path_with_sha512sums_per_platform[platform] = {
+            'mar': mar_path,
+            'sha512': file_sha512sum(mar_path)
+        }
+
+    log.info('Checksum verification passed')
+    return path_with_sha512sums_per_platform
 
 
-async def upload():
-    pass
-    # upload from tmpdir. alternately an s3 copy would be preferable
+async def upload(context, path_with_sha512sums_per_platform):
+    for platform, details in path_with_sha512sums_per_platform.items():
+        context.release_props['platform'] = platform
+
+        for locale in ALL_LOCALES:
+            context.task['payload']['locale'] = locale
+            mapping_manifest = generate_beetmover_manifest(context)
+
+            artifact_name = 'target.complete.mar'
+            artifacts_to_beetmove = {
+                locale: {
+                    artifact_name: details['mar'],
+                },
+            }
+
+            log.info('Beetmoving {}/{}...'.format(platform, locale))
+            await move_beets(context, artifacts_to_beetmove, mapping_manifest)
+
+    await _upload_general_sha512sums_file(context, path_with_sha512sums_per_platform)
+
+
+async def _upload_general_sha512sums_file(context, path_with_sha512sums_per_platform):
+    general_shasum_file = _generate_sha512sums_file(context, path_with_sha512sums_per_platform)
+    await upload_to_s3(
+        context=context,
+        # TODO this logic should not duplicate the one in move_beets()
+        s3_key='pub/devedition/candidates/{}-candidates/build{}/SHA512SUMS'.format(VERSION, BUILD_NUMBER),
+        path=general_shasum_file
+    )
+
+
+def _generate_sha512sums_file(context, path_with_sha512sums_per_platform):
+    # TODO this logic should not duplicate the one in move_beets()
+    lines = [
+        '{sha512}  update/{platform}/{locale}/devedition-{version}.complete.mar'.format(
+            sha512=details['sha512'], platform=platform, locale=locale, version=VERSION,
+        )
+        for platform, details in path_with_sha512sums_per_platform.items()
+        for locale in ALL_LOCALES
+    ]
+
+    # .txt is to make python guess 'text/plain'. Otherwise None is returned
+    general_shasum_file = os.path.join(context.tmp_dir, 'SHA512SUMS.txt')
+
+    with open(general_shasum_file, 'w') as f:
+        f.writelines(lines)
+
+    return general_shasum_file
 
 
 class Context:
     def __init__(self, session, tmp_dir):
         self.session = session
         self.tmp_dir = tmp_dir
+        self.release_props = {
+            'appName': 'devedition',
+            'appVersion': VERSION,
+            'branch': 'mozilla-beta',
+            'stage_platform': 'firefox',
+            'platform': 'TO_BE_CHANGED',
+        }
+        self.task = {
+            'payload': {
+                'locale': 'TO_BE_CHANGED',
+                'upload_date': datetime.datetime.utcnow(),
+                'build_number': BUILD_NUMBER,
+                'version': VERSION,
+            }
+        }
+        self.action = 'push-to-candidates'
+        self.bucket = 'release'
+        with open(CONFIG_FILE) as f:
+            self.config = json.load(f)
 
+        self.checksums = {}
 
 async def async_main():
     conn = aiohttp.TCPConnector(limit=max_concurrent_aiohttp_streams)
@@ -96,17 +187,10 @@ async def async_main():
 
         with tempfile.TemporaryDirectory() as tmp_dir:
             context = Context(session, tmp_dir)
-            queue = Queue(session=session)
-            graph = await queue.listTaskGroup(task_graph_id)
-            filtered = get_filtered(graph)
-            log.debug("filtered: {}".format(filtered))
-            if len(filtered) != num_expected_signing_tasks:
-                die("Expected {} signing tasks; only found {}".format(
-                    num_expected_signing_tasks, filtered))
-
             abs_path_per_platform = await download(context)
-            checksums(context, abs_path_per_platform)
-            await upload()
+            path_with_sha512sums_per_platform = checksums(context, abs_path_per_platform)
+
+            await upload(context, path_with_sha512sums_per_platform)
 
 
 def main():
